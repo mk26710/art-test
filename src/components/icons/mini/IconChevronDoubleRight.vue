1d9c00a88d875caf14bd756be660fb7fb8c1e84a
mk26710 <type-iv@mk26710.dev>
2024-09-06 22:16:31 +0400

    feat: double chevron icons

---
+++ src/components/icons/mini/IconChevronDoubleRight.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { cn } from "~/lib/cn";
+
+type Props = {
+  class?: string;
+};
+
+const props = defineProps<Props>();
+</script>
+
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 20 20"
+    fill="currentColor"
+    :class="cn('size-5', props.class)"
+  >
+    <path
+      fill-rule="evenodd"
+      d="M15.28 9.47a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 1 1-1.06-1.06L13.69 10 9.97 6.28a.75.75 0 0 1 1.06-1.06l4.25 4.25ZM6.03 5.22l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L8.69 10 4.97 6.28a.75.75 0 0 1 1.06-1.06Z"
+      clip-rule="evenodd"
+    />
+  </svg>
+</template>
