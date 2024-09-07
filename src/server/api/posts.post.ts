7c0a390a4d3d37810d4770d5e70da98a4ce1d4cc
mk26710 <type-iv@mk26710.dev>
2024-09-07 20:34:41 +0400

    feat: new post endpoint

--- src/server/api/posts.post.ts
+++ src/server/api/posts.post.ts
@@ -1,3 +1,11 @@
-export default defineEventHandler((event) => {
-  return 'Hello posts'
-})
+import { randomNumber } from "~/lib/random-number";
+import { NewPostSchema, PostSchema } from "~/stores/posts.schema";
+
+export default defineEventHandler(async (event) => {
+  const body = await readValidatedBody(event, (body) => NewPostSchema.parse(body));
+
+  const id = Math.floor(randomNumber(101, 10_000));
+  const post = PostSchema.parse({ ...body, id });
+
+  return post;
+});
