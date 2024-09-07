6d3e9f857f357b25d1dacc8e2a49809de2eda73f
mk26710 <type-iv@mk26710.dev>
2024-09-07 20:00:22 +0400

    feat: h4 for typography

---
+++ src/components/ui/typography/H4.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { cn } from "~/lib/cn";
+
+const props = defineProps({
+  class: {
+    type: String,
+    required: false,
+    default: "",
+  },
+});
+</script>
+
+<template>
+  <h4 :class="cn('scroll-m-20 text-xl font-semibold tracking-tight', props.class)">
+    <slot />
+  </h4>
+</template>
