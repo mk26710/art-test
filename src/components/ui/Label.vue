72b8b85e773d13252361cbe68a16334ccba24501
mk26710 <type-iv@mk26710.dev>
2024-09-07 20:00:57 +0400

    feat: label component

---
+++ src/components/ui/Label.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { cn } from "~/lib/cn";
+
+type Props = {
+  class?: string;
+};
+
+const props = defineProps<Props>();
+</script>
+
+<template>
+  <label
+    :class="
+      cn(
+        'text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70',
+        props.class,
+      )
+    "
+  >
+    <slot />
+  </label>
+</template>
