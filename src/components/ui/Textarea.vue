879303931f7e3abd116f7c55a74e5c58a72c40a2
mk26710 <type-iv@mk26710.dev>
2024-09-07 20:00:48 +0400

    feat: textarea component

---
+++ src/components/ui/Textarea.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { cn } from "~/lib/cn";
+
+type Props = {
+  class?: string;
+};
+
+const props = defineProps<Props>();
+const modelValue = defineModel<string>();
+</script>
+
+<template>
+  <textarea
+    v-model="modelValue"
+    :class="
+      cn(
+        'border-input placeholder:text-muted-foreground focus-visible:ring-ring flex min-h-[60px] w-full rounded-md border bg-transparent px-3 py-2 text-sm shadow-sm focus-visible:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50',
+        props.class,
+      )
+    "
+  />
+</template>
