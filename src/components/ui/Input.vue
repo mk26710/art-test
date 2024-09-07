113dea25dd0399fe4d1de049fd4ea762c44f2ace
mk26710 <type-iv@mk26710.dev>
2024-09-07 20:00:36 +0400

    feat: input component

--- src/components/ui/Input.vue
+++ src/components/ui/Input.vue
@@ -6,15 +6,17 @@ type Props = {
 };
 
 const props = defineProps<Props>();
-const model = defineModel<string>({ required: false });
+const model = defineModel<string | number>({ required: false });
 </script>
 
+<!-- eslint-disable vue/html-self-closing -->
+<!-- above is disabled because prettier formatter is annoying about this and will not let you have no / at the closing brace -->
 <template>
   <input
     v-model="model"
     :class="
       cn(
-        'flex h-10 w-full rounded-md border bg-white px-3 py-2 text-sm ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-neutral-300 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-neutral-300 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+        'h-9 flex w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50',
         props.class,
       )
     "
